a50da47dcdbdff5ef52f8c88895c2eb1b97da7ba
XymenApp <xymenapps.official@gmail.com>
2020-08-02 03:30:41 +0530

    added bans url advert

--- cronjobs-update.sh
+++ cronjobs-update.sh
@@ -16,8 +16,8 @@ echo "-----------------------------------------" >> webserver/logs/update.log
 echo "-----------------------------------------" >> webserver/logs/update.log
 echo "cronjobs-update started at `date`" >> webserver/logs/update.log
 ./csgoserver update >> webserver/logs/update.log
-./csgoserver-2 r >> webserver/logs/update.log
-./csgoserver-3 r >> webserver/logs/update.log
+./csgoserver-2 force-update >> webserver/logs/update.log
+./csgoserver-3 force-update >> webserver/logs/update.log
 echo "cronjobs-update finished at `date`" >> webserver/logs/update.log
 echo "-----------------------------------------" >> webserver/logs/update.log
 echo "-----------------------------------------" >> webserver/logs/update.log
